d3428234d36d392d8c2a414836101e39b030e6c4
Jay R Stewart <jayr.stewart@live.com>
2020-11-27 20:09:38 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func Generate(ch chan<- int) {
+	for i := 2; ; i++ {
+		ch <- i // Send 'i' to channel 'ch'.
+	}
+}
+
+func Filter(in <-chan int, out chan<- int, prime int) {
+	for {
+		i := <-in // Receive value from 'in'.
+		if i%prime != 0 {
+			out <- i // Send 'i' to 'out'.
+		}
+
+	}
+}
+
+var nthPrime int = 10000
+
+func main() {
+
+	var prime int
+	ch := make(chan int)
+	go Generate(ch)
+	for i := 0; i <= nthPrime; i++ {
+		prime = <-ch
+		ch1 := make(chan int)
+		go Filter(ch, ch1, prime)
+		ch = ch1
+	}
+	fmt.Printf("Last element: %v\n", prime)
+}
